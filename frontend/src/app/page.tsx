0b935c2d1cde0d6083043d58ff6d33a2a4ae81c5
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-16 10:11:42 +0100

    add test button

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,23 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const [test, setTest] = useState();
+
+  const getTest = async () => {
+    const response = await fetch(
+      "https://figure-forge-shop-app.azurewebsites.net/client"
+    );
+    setTest(await response.json());
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Typo titans</h1>
       <h1>frontpage</h1>
+      <button onClick={getTest}>test</button>
+      {test && <h1>{test}</h1>}
     </main>
   );
 }
