42c60ddb72892f9fbe3503fe0345d87b6e76b2b3
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-16 09:52:51 +0100

    add backend

---
+++ backend/src/main/java/com/typotitans/backend/controllers/ClientController.java
@@ -0,0 +1,16 @@
+package com.typotitans.backend.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("client")
+public class ClientController {
+
+    @GetMapping
+    ResponseEntity<String> Test () {
+        return ResponseEntity.ok("Test");
+    }
+}
