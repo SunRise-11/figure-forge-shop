52cf047c31c230a7ee0e7089f0b768e77be98825
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-16 09:41:17 +0100

    add frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>HELLO TO TYPO TITANS</h1>
-      <h1>Client Frontend</h1>
+      <h1>Typo titans</h1>
+      <h1>frontpage</h1>
     </main>
   );
 }
