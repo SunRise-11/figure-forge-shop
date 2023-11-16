1343835be20ce1a456ed0075a801f1a75a4caae8
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-16 10:15:23 +0100

    add backend

--- backend/src/main/java/com/typotitans/backend/controllers/ClientController.java
+++ backend/src/main/java/com/typotitans/backend/controllers/ClientController.java
@@ -1,12 +1,14 @@
 package com.typotitans.backend.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("client")
+@CrossOrigin
 public class ClientController {
 
     @GetMapping
