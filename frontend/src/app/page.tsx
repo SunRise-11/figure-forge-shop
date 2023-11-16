771f7f1e76057b30f91d5c5ff31e3881100d8f32
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-16 10:28:05 +0100

    change to axios

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,22 +1,23 @@
 "use client";
 import Image from "next/image";
 import { useState } from "react";
+import axios from "axios";
 
 export default function Home() {
   const [test, setTest] = useState();
 
   const getTest = async () => {
-    const response = await fetch(
+    const response = await axios.get(
       "https://figure-forge-shop-app.azurewebsites.net/client"
     );
-    setTest(await response.json());
+    setTest(await response.data);
   };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Typo titans</h1>
       <h1>frontpage</h1>
-      <button onClick={getTest}>test</button>
+      <button onClick={getTest}>test 1</button>
       {test && <h1>{test}</h1>}
     </main>
   );
