14703d6200ec8c53954baba35d8e0c50dfff5d52
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-20 15:42:56 +0100

    Implement public POST method for creating new figure

--- backend/src/main/java/com/typotitans/backend/controllers/PublicController.java
+++ backend/src/main/java/com/typotitans/backend/controllers/PublicController.java
@@ -1,10 +1,13 @@
 package com.typotitans.backend.controllers;
 
+import com.typotitans.backend.dtos.FigureDto;
 import com.typotitans.backend.models.Figure;
 import com.typotitans.backend.services.FigureService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -19,13 +22,22 @@ public class PublicController {
     }
 
     @GetMapping
-    ResponseEntity<List<Figure>> getAllFigures () {
+    ResponseEntity<List<Figure>> getAllFigures() {
         return ResponseEntity.ok(figureService.getAllFigures());
     }
 
     @GetMapping("{id}")
-    ResponseEntity<Figure> getSpecificFigure (@PathVariable String id) {
+    ResponseEntity<Figure> getSpecificFigure(@PathVariable String id) {
         var figure = figureService.getFigure(id);
         return ResponseEntity.ok(figure);
+    }
+
+    @PostMapping
+    ResponseEntity<Figure> createFigure(@RequestBody FigureDto body, HttpServletRequest req) {
+
+        var figure = figureService.addFigure(body);
+
+        URI location = URI.create(req.getRequestURI() + "/" + figure.getId());
+        return ResponseEntity.created(location).body(figure);
     }
 }
