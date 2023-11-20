95ed051c6c0f1391491980de71903f0b19446dd0
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-20 11:32:38 +0100

    Implement getFigure and getAllFigures methods

--- backend/src/main/java/com/typotitans/backend/services/FigureService.java
+++ backend/src/main/java/com/typotitans/backend/services/FigureService.java
@@ -1,7 +1,26 @@
 package com.typotitans.backend.services;
 
+import com.typotitans.backend.models.Figure;
+import com.typotitans.backend.repositories.FigureRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class FigureService {
+    private final FigureRepository figureRepository;
+
+    public FigureService(FigureRepository figureRepository) {
+        this.figureRepository = figureRepository;
+    }
+
+
+    public List<Figure> getAllFigures () {
+        return figureRepository.findAll();
+    }
+
+    public Figure getFigure (String id) {
+        return figureRepository.findById(id).orElseThrow(() -> new NoSuchElementException("Figure not found"));
+    }
 }
