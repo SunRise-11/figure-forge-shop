dea1a256696e9fb07013b0dea895335dcddff63c
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-20 13:00:31 +0100

    fix import

--- frontend/src/app/(public)/about/page.tsx
+++ frontend/src/app/(public)/about/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import Banner from "../components/public/Banner"; 
-
+import Banner from "../../components/public/Banner";
 
 const AboutPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between ">
-      <Banner name="ABOUT US" imageSource="https://daweebstop.com/cdn/shop/files/IMG_0171_c8d72572-d4b5-42b3-a240-b9bc2cc2aca9.jpg?v=1661460103" />
+      <Banner
+        name="ABOUT US"
+        imageSource="https://daweebstop.com/cdn/shop/files/IMG_0171_c8d72572-d4b5-42b3-a240-b9bc2cc2aca9.jpg?v=1661460103"
+      />
       <div className="my-10">
         <p>
           At Figure Forge, our story is a tapestry woven with threads of passion
