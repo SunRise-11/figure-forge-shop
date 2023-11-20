b596ccb66bdba0e5aabe42ee70e2e54d928fca0f
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-20 17:56:11 +0100

    Refactor Dto to handle MultiPart files

--- backend/src/main/java/com/typotitans/backend/dtos/FigureDto.java
+++ backend/src/main/java/com/typotitans/backend/dtos/FigureDto.java
@@ -2,13 +2,14 @@ package com.typotitans.backend.dtos;
 
 import com.typotitans.backend.models.Picture;
 import com.typotitans.backend.models.Seller;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public record FigureDto(
         String name,
         String description,
-        List<Picture> pictures,
+        List<MultipartFile> pictures,
         String brand,
         double price,
         String origin,
