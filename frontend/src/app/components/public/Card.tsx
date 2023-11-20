5b384a500887491add016a8fa946a86ce7631f38
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-20 10:26:54 +0100

    add card

--- frontend/src/app/components/public/Card.tsx
+++ frontend/src/app/components/public/Card.tsx
@@ -1,7 +1,47 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
-const Card = () => {
-  return <div>Card</div>;
+export type Toy = {
+  id: Number;
+  name: String;
+  origin: String;
+  brand: String;
+  price: Number;
+  width: Number;
+  diameter: Number;
+  height: Number;
+  rating: number;
+  description: String;
+  detail: String;
+  pictures: String[];
+};
+
+type Props = {
+  toy: Toy;
+};
+
+const Card = ({ toy }: Props) => {
+  return (
+    <Link
+      href={`/shop/${toy.id}`}
+      className="w-[300px] h-[380px] bg-purple border-4 border-white flex flex-col justify-between rounded-lg sm:my-10"
+    >
+      <div className="w-full h-[300px] border-white flex item-center justify-center overflow-hidden rounded-t-lg">
+        <Image
+          className="h-full"
+          src={toy.pictures[0].toString()}
+          alt="some Pic"
+          width={300}
+          height={500}
+        />
+      </div>
+      <div className="px-4 py-2 my-0">
+        <h3>{toy.name}</h3>
+        <h3>$ {toy.price.toString()}</h3>
+      </div>
+    </Link>
+  );
 };
 
 export default Card;
