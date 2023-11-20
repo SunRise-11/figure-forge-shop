a65f3430b9a0f96115bcda8688564b2822369e28
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-20 16:27:16 +0100

    Add FigureBuilder

---
+++ backend/src/main/java/com/typotitans/backend/models/FigureBuilder.java
@@ -0,0 +1,71 @@
+package com.typotitans.backend.models;
+
+import java.util.List;
+
+public class FigureBuilder {
+    private String name;
+    private String origin;
+    private String brand;
+    private double price;
+    private int width;
+    private int length;
+    private int height;
+    private int weight;
+    private String description;
+    private List<Picture> pictures;
+
+    public FigureBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public FigureBuilder setOrigin(String origin) {
+        this.origin = origin;
+        return this;
+    }
+
+    public FigureBuilder setBrand(String brand) {
+        this.brand = brand;
+        return this;
+    }
+
+    public FigureBuilder setPrice(double price) {
+        this.price = price;
+        return this;
+    }
+
+    public FigureBuilder setWidth(int width) {
+        this.width = width;
+        return this;
+    }
+
+    public FigureBuilder setLength(int length) {
+        this.length = length;
+        return this;
+    }
+
+    public FigureBuilder setHeight(int height) {
+        this.height = height;
+        return this;
+    }
+
+    public FigureBuilder setWeight(int weight) {
+        this.weight = weight;
+        return this;
+    }
+
+    public FigureBuilder setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public FigureBuilder setPictures(List<Picture> pictures) {
+        this.pictures = pictures;
+        return this;
+    }
+
+    public Figure createFigure() {
+        return new Figure(name, origin, brand, price, width, length, height, weight, description,
+                pictures);
+    }
+}
