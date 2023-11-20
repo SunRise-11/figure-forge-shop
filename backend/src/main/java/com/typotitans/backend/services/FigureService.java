67cd211fc3b26fcfd8a1d77c4c3623b116a19519
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-20 17:57:02 +0100

    Implement savePictureAsBlob method

--- backend/src/main/java/com/typotitans/backend/services/FigureService.java
+++ backend/src/main/java/com/typotitans/backend/services/FigureService.java
@@ -2,23 +2,42 @@ package com.typotitans.backend.services;
 
 import com.typotitans.backend.dtos.FigureDto;
 import com.typotitans.backend.models.Figure;
+import com.typotitans.backend.models.Picture;
 import com.typotitans.backend.repositories.FigureRepository;
+import com.typotitans.backend.repositories.PictureRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.NoSuchElementException;
 
 @Service
 public class FigureService {
     private final FigureRepository figureRepository;
+    private final PictureRepository pictureRepository;
 
-    public FigureService(FigureRepository figureRepository) {
+    public FigureService(FigureRepository figureRepository, PictureRepository pictureRepository) {
         this.figureRepository = figureRepository;
+        this.pictureRepository = pictureRepository;
     }
 
     private Figure convertDtoToEntity(FigureDto dto) {
         return new Figure(dto.name(), dto.origin(), dto.brand(), dto.price(), dto.width(),
-                dto.length(), dto.height(), dto.weight(), dto.description(), dto.pictures(), dto.seller());
+                dto.length(), dto.height(), dto.weight(), dto.description(), dto.seller());
+
+
+    }
+
+    private Picture savePictureAsBlob(MultipartFile image, Figure figure) {
+        try {
+            Picture picture = new Picture(image.getBytes(),figure);
+            pictureRepository.save(picture);
+            return picture;
+            } catch (IOException exception) {
+            System.out.println("Could not save image");
+            return null;
+        }
     }
 
 
@@ -33,6 +52,7 @@ public class FigureService {
 
     public Figure addFigure(FigureDto request) {
         var figure = convertDtoToEntity(request);
+            request.pictures().stream().map(image -> savePictureAsBlob(image, figure));
         figureRepository.save(figure);
 
         return figure;
