4e6199360d3466528bfaeb2df12eceff48861342
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-20 14:46:51 +0100

    Implement addFigure method

--- backend/src/main/java/com/typotitans/backend/services/FigureService.java
+++ backend/src/main/java/com/typotitans/backend/services/FigureService.java
@@ -1,7 +1,10 @@
 package com.typotitans.backend.services;
 
+import com.typotitans.backend.dtos.FigureDto;
 import com.typotitans.backend.models.Figure;
 import com.typotitans.backend.repositories.FigureRepository;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,10 +12,17 @@ import java.util.NoSuchElementException;
 
 @Service
 public class FigureService {
+    private final ModelMapper modelMapper;
     private final FigureRepository figureRepository;
 
-    public FigureService(FigureRepository figureRepository) {
+    public FigureService(ModelMapper modelMapper, FigureRepository figureRepository) {
+        this.modelMapper = modelMapper;
         this.figureRepository = figureRepository;
+    }
+
+    private Figure convertDtoToEntity(FigureDto dto) {
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STANDARD);
+        return modelMapper.map(dto, Figure.class);
     }
 
 
@@ -22,5 +32,12 @@ public class FigureService {
 
     public Figure getFigure (String id) {
         return figureRepository.findById(id).orElseThrow(() -> new NoSuchElementException("Figure not found"));
+    }
+
+    public Figure addFigure (FigureDto request) {
+        var figure = convertDtoToEntity(request);
+        figureRepository.save(figure);
+
+        return figure;
     }
 }
