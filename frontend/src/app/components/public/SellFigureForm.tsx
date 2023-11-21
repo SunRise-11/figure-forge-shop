0f905ef979507ec66fabe8cbbd31ade7eacbd1e8
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-21 14:28:59 +0100

    Add basic styling to SellFigureForm

--- frontend/src/app/components/public/SellFigureForm.tsx
+++ frontend/src/app/components/public/SellFigureForm.tsx
@@ -11,44 +11,83 @@ export const SellFigureForm = () => {
     httpPostFigure(data);
   });
 
+  const inputStyle =
+    'bg-secondary appearance-none border-2 border-secondary rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:border-primary';
+  const fieldStyle = 'flex flex-col gap-5 h-max mb-4 sm:w-1/2' 
   return (
-    <form onSubmit={onFormSubmit}>
-      <input {...register('name')} placeholder="Name" />
-      <input {...register('origin')} placeholder="Origin" />
-      <input {...register('brand')} placeholder="Brand" />
+    <form
+      className="flex flex-col items-center gap-5 h-max mb-4 py-10 "
+      onSubmit={onFormSubmit}
+    >
+      <legend>Details</legend>
+      <fieldset className={fieldStyle}>
+        <input
+          className={inputStyle}
+          {...register('name')}
+          placeholder="Name"
+        />
+        <input
+          className={inputStyle}
+          {...register('origin')}
+          placeholder="Origin"
+        />
+        <input
+          className={inputStyle}
+          {...register('brand')}
+          placeholder="Brand"
+        />
+        <textarea 
+          className={inputStyle}
+          {...register('description')}
+          placeholder="Description"
+        />
+        <input
+          className={inputStyle}
+          {...register('price', { valueAsNumber: true })}
+          type="number"
+          step="0.01"
+          placeholder="Price (EUR)"
+        />
+      </fieldset>
+
+      <legend>Dimensions</legend>
+      <fieldset className={fieldStyle}>
+        <input
+          className={inputStyle}
+          {...register('width', { valueAsNumber: true })}
+          type="number"
+          placeholder="Width (cm)"
+        />
+        <input
+          className={inputStyle}
+          {...register('length', { valueAsNumber: true })}
+          type="number"
+          placeholder="Length (cm)"
+        />
+        <input
+          className={inputStyle}
+          {...register('height', { valueAsNumber: true })}
+          type="number"
+          placeholder="Height (cm)"
+        />
+        <input
+          className={inputStyle}
+          {...register('weight', { valueAsNumber: true })}
+          type="number"
+          placeholder="Weight (g)"
+        />
+      </fieldset>
+
+      <legend>Pictures</legend>
+      <fieldset className={fieldStyle}>
       <input
-        {...register('width', { valueAsNumber: true })}
-        type="number"
-        placeholder="Width"
+        className={inputStyle}
+        type="file"
+        {...register('pictures')}
+        multiple
       />
-      <input
-        {...register('length', { valueAsNumber: true })}
-        type="number"
-        placeholder="Length"
-      />
-      <input
-        {...register('height', { valueAsNumber: true })}
-        type="number"
-        placeholder="Height"
-      />
-      <input
-        {...register('weight', { valueAsNumber: true })}
-        type="number"
-        placeholder="Weight"
-      />
-      <input {...register('description')} placeholder="Description" />
-      <input
-        {...register('price', { valueAsNumber: true })}
-        type="number"
-        step="0.01"
-        placeholder="Price"
-      />
-      <input
-        {...register('rating', { valueAsNumber: true })}
-        type="number"
-        placeholder="Rating"
-      />
-      <input type="file" {...register('pictures')} multiple />
+      </fieldset>
+
       <button type="submit">Submit</button>
     </form>
   );
