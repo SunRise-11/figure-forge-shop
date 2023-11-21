cc3270bfba604065d123121f91dde26dc868d148
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-21 11:09:23 +0100

    added request file for http calls

---
+++ frontend/src/app/api/http/requests.ts
@@ -0,0 +1,22 @@
+const API_URI = "http://localhost:8080/public/figures";
+
+export const httpGetAllFigures = async () => {
+    const response = await fetch(API_URI);
+    return response.json();
+}
+
+export const httpPostFigure = async (figure:any) => {
+    return await fetch(API_URI, {
+        method: "POST",
+        headers: {
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify(figure)
+    })
+}
+
+export const httpDeleteFigure = async (id: Number) => {
+    return await fetch(`${API_URI}/${id}`,{
+        method:'DELETE'
+    });
+}
