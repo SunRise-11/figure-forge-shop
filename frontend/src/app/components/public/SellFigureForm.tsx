cc731496270d928e21e0b415900b2ac589951c0d
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-21 11:47:24 +0100

    Add SellFigureForm

---
+++ frontend/src/app/components/public/SellFigureForm.tsx
@@ -0,0 +1,54 @@
+import { httpPostFigure } from '@/app/api/http/requests';
+import { Figure } from '@/types';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+export const SellFigureForm = () => {
+  const { register, handleSubmit } = useForm<Figure>();
+
+  const onFormSubmit = handleSubmit((data) => {
+    httpPostFigure(data);
+  });
+
+  return (
+    <form onSubmit={onFormSubmit}>
+      <input {...register('name')} placeholder="Name" />
+      <input {...register('origin')} placeholder="Origin" />
+      <input {...register('brand')} placeholder="Brand" />
+      <input
+        {...register('width', { valueAsNumber: true })}
+        type="number"
+        placeholder="Width"
+      />
+      <input
+        {...register('length', { valueAsNumber: true })}
+        type="number"
+        placeholder="Length"
+      />
+      <input
+        {...register('height', { valueAsNumber: true })}
+        type="number"
+        placeholder="Height"
+      />
+      <input
+        {...register('weight', { valueAsNumber: true })}
+        type="number"
+        placeholder="Weight"
+      />
+      <input {...register('description')} placeholder="Description" />
+      <input
+        {...register('price', { valueAsNumber: true })}
+        type="number"
+        step="0.01"
+        placeholder="Price"
+      />
+      <input
+        {...register('rating', { valueAsNumber: true })}
+        type="number"
+        placeholder="Rating"
+      />
+      <input type="file" {...register('pictures')} multiple />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
