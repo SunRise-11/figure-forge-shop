04735f0eba73c532934af6f04ffe74c92e95db54
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-22 15:32:31 +0100

    Add functionality to get pictures by figure id

--- backend/src/main/java/com/typotitans/backend/repositories/PictureRepository.java
+++ backend/src/main/java/com/typotitans/backend/repositories/PictureRepository.java
@@ -3,5 +3,9 @@ package com.typotitans.backend.repositories;
 import com.typotitans.backend.models.Picture;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.List;
+
 public interface PictureRepository extends ListCrudRepository<Picture, String> {
+    List<Picture> findAllByFigureId(String figureId);
+
 }
