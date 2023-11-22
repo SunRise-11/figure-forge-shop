1bc32f4d0ecf34d3f6e9a429005759b6e6bafdfe
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-22 09:31:03 +0100

    Implement POST method for creating new figure

--- backend/src/main/java/com/typotitans/backend/controllers/PublicController.java
+++ backend/src/main/java/com/typotitans/backend/controllers/PublicController.java
@@ -1,11 +1,14 @@
 package com.typotitans.backend.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.typotitans.backend.dtos.FigureDto;
 import com.typotitans.backend.models.Figure;
 import com.typotitans.backend.services.FigureService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.net.URI;
 import java.util.List;
@@ -16,9 +19,11 @@ import java.util.List;
 public class PublicController {
 
     private final FigureService figureService;
+    private final ObjectMapper objectMapper;
 
-    public PublicController(FigureService figureService) {
+    public PublicController(FigureService figureService, ObjectMapper objectMapper) {
         this.figureService = figureService;
+        this.objectMapper = objectMapper;
     }
 
     @GetMapping
@@ -33,11 +38,14 @@ public class PublicController {
     }
 
     @PostMapping
-    ResponseEntity<Figure> createFigure(@RequestBody FigureDto body, HttpServletRequest req) {
+    ResponseEntity<FigureDto> createFigure(@RequestPart(value = "pictures") MultipartFile[] pictures,
+                                        @RequestPart(value = "figureDetails") String figureDetails,
+                                        HttpServletRequest req) throws JsonProcessingException {
+        FigureDto dto = objectMapper.readValue(figureDetails, FigureDto.class);
 
-        var figure = figureService.addFigure(body);
+        var figure = figureService.addFigure(dto, pictures);
 
-        URI location = URI.create(req.getRequestURI() + "/" + figure.getId());
+        URI location = URI.create(req.getRequestURI() + "/" + figure.id());
         return ResponseEntity.created(location).body(figure);
     }
 }
