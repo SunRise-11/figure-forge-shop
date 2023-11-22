8894d52fa1434e134f8ea3c90972629152f9d51b
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-22 14:37:31 +0100

    load data from context for all figures in admin

--- frontend/src/app/(admin)/dashboard/figures/page.tsx
+++ frontend/src/app/(admin)/dashboard/figures/page.tsx
@@ -1,12 +1,24 @@
 "use client";
 
-import React from "react";
+import React, { useContext, useState } from "react";
 import { Card, Typography } from "@material-tailwind/react";
- 
-  
+import { FiguresContext } from "@/app/contexts/figures.context";
+
 const AllFigures = () => {
+  const { toys } = useContext(FiguresContext);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const elementsPerPage: number = 9;
+
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const startIndex = (currentPage - 1) * elementsPerPage;
+  const endIndex = startIndex + elementsPerPage;
+  const displayedToys = toys.slice(startIndex, endIndex);
+
   return (
-    <main className="w-screen h-screen bg-background flex items-center justify-center mt-10">
+    <main className="w-screen h-screen bg-background flex items-center justify-center mt-0">
       <Card className="absolute right-0 w-full max-w-[calc(100vw-19rem)] h-max p-4 shadow-xl shadow-blue-gray-900/5 border-solid border-2">
         <Typography variant="h5" color="blue-gray" className=" my-auto px-10">
           All Figures
@@ -27,18 +39,16 @@ const AllFigures = () => {
                 Price
               </th>
             </tr>
-            <tr>
-              <td className="border px-6 py-4">name 1</td>
-              <td className="border px-6 py-4">status 1</td>
-              <td className="border px-6 py-4">Rating 1</td>
-              <td className="border px-6 py-4">Price 1</td>
-            </tr>
-            <tr>
-              <td className="border px-6 py-4">name 2</td>
-              <td className="border px-6 py-4">status 2</td>
-              <td className="border px-6 py-4">Rating 2</td>
-              <td className="border px-6 py-4">Price 2</td>
-            </tr>
+            {displayedToys.map((toy) => {
+              return (
+                <tr>
+                  <td className="border px-6 py-4">{toy.name}</td>
+                  <td className="border px-6 py-4">{toy.origin}</td>
+                  <td className="border px-6 py-4">{toy.rating}</td>
+                  <td className="border px-6 py-4">{toy.price.toString()}</td>
+                </tr>
+              );
+            })}
           </thead>
         </table>
       </Card>
