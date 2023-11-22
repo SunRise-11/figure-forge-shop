72153928a4b86ff91e1517439047fd89ba814b18
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-22 15:36:05 +0100

    Implement logic to get all figures and pictures

--- backend/src/main/java/com/typotitans/backend/controllers/PublicController.java
+++ backend/src/main/java/com/typotitans/backend/controllers/PublicController.java
@@ -3,9 +3,11 @@ package com.typotitans.backend.controllers;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.typotitans.backend.dtos.FigureDto;
+import com.typotitans.backend.dtos.ResponseDto;
 import com.typotitans.backend.models.Figure;
 import com.typotitans.backend.services.FigureService;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -27,8 +29,17 @@ public class PublicController {
     }
 
     @GetMapping
-    ResponseEntity<List<Figure>> getAllFigures() {
-        return ResponseEntity.ok(figureService.getAllFigures());
+    ResponseEntity<List<ResponseDto>> getAllFigures() {
+        var figures = figureService.getAllFigures();
+//        ResponseDto response = new ResponseDto(objectMapper.convertValue(fig))
+        var response = figures.stream().map(figure -> {
+            var pictures = figure.getPictures();
+            return new ResponseDto(objectMapper.convertValue(figure, FigureDto.class),
+                    pictures);
+
+        }).toList();
+
+        return ResponseEntity.ok().body(response);
     }
 
     @GetMapping("{id}")
