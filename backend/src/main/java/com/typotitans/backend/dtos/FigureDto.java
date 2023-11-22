87755ef9f616ad265ee0e2d5eb11ff35b2fd2ff5
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-22 15:29:50 +0100

    Remove pictures from FigureDto

--- backend/src/main/java/com/typotitans/backend/dtos/FigureDto.java
+++ backend/src/main/java/com/typotitans/backend/dtos/FigureDto.java
@@ -2,8 +2,6 @@ package com.typotitans.backend.dtos;
 
 import com.typotitans.backend.models.Seller;
 
-import java.util.List;
-
 public record FigureDto(
         String id,
         String name,
@@ -15,7 +13,6 @@ public record FigureDto(
         int height,
         int length,
         int weight,
-        Seller seller,
-        List<String> pictures
+        Seller seller
 ) {
 }
