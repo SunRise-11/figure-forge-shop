cf59a855c0cb2bb466060228de8655b85a003ec2
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-23 16:19:45 +0100

    import allfigures in dashboard page

--- frontend/src/app/(admin)/dashboard/page.tsx
+++ frontend/src/app/(admin)/dashboard/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import AllFigures from "@/app/components/admin/allfigures";
  import Card from "@/app/components/admin/Card";
 import { FiguresContext } from "@/app/contexts/figures.context";
 import { withPageAuthRequired, getSession } from "@auth0/nextjs-auth0";
@@ -15,7 +16,7 @@ const Dashboard = () => {
         <Card title="Uncheck" />
       </div>
       <div className="w-3/4 absolute left-[19rem] top-60 flex flex-row gap-10 mt-10">
- 
+        <AllFigures />
        </div>
     </div>
   );
