cf3f65f26d0d0d5fb46a4cb06bdbe3f885efc115
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-23 19:48:26 +0100

    change topbar text size

--- frontend/src/app/components/admin/Topbar.tsx
+++ frontend/src/app/components/admin/Topbar.tsx
@@ -8,7 +8,7 @@ const Topbar = () => {
   const path = pathname.split("/");
   return (
     <Card className="absolute top-0 right-0 w-full max-w-[calc(100vw-19rem)] h-20 p-4 shadow-xl shadow-blue-gray-900/5 border-solid border-2">
-      <Typography variant="h5" color="blue-gray" className=" my-auto px-10">
+      <Typography variant="h2" color="blue-gray" className=" my-auto">
         {path[path.length - 1]}
       </Typography>
     </Card>
