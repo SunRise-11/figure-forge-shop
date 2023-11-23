cc0513d1208582515418a2141c7f26b89352c600
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-23 19:18:34 +0100

    remove my-auto from title

--- frontend/src/app/components/admin/Figures.tsx
+++ frontend/src/app/components/admin/Figures.tsx
@@ -27,7 +27,7 @@ const Figures = ({ action, data }: Props) => {
   }
   return (
     <Card className="absolute right-0 top-[6rem] w-full max-w-[calc(100vw-19rem)] overflow-y-auto h-[calc(100vh-6rem)] p-4 shadow-xl shadow-blue-gray-900/5 border-solid border-2 ">
-      <Typography variant="h5" color="blue-gray" className=" my-auto ">
+      <Typography variant="h5" color="blue-gray">
         {action} Figures
       </Typography>
       <table className="w-full text-sm text-left text-black mt-10">
