9eb4e8e00fd5650a978960706f65860d38d90664
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-23 17:17:13 +0100

    Refactor addFigure to save pictures to blob storage

--- backend/src/main/java/com/typotitans/backend/services/FigureService.java
+++ backend/src/main/java/com/typotitans/backend/services/FigureService.java
@@ -1,27 +1,32 @@
 package com.typotitans.backend.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.typotitans.backend.dtos.FigureDto;
-import com.typotitans.backend.dtos.ResponseDto;
-import com.typotitans.backend.dtos.UpdateDto;
 import com.typotitans.backend.models.Figure;
+import com.typotitans.backend.models.Picture;
 import com.typotitans.backend.repositories.FigureRepository;
+import com.typotitans.backend.repositories.PictureRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Base64;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.NoSuchElementException;
 
 @Service
 public class FigureService {
     private final FigureRepository figureRepository;
-    private final PictureService pictureService;
+    private final PictureRepository pictureRepository;
     private final ObjectMapper objectMapper;
+    private final BlobService blobService;
 
-    public FigureService(FigureRepository figureRepository, PictureService pictureService,
-                         ObjectMapper objectMapper) {
+    public FigureService(FigureRepository figureRepository,
+                         PictureRepository pictureRepository, ObjectMapper objectMapper, BlobService blobService) {
         this.figureRepository = figureRepository;
-        this.pictureService = pictureService;
+        this.pictureRepository = pictureRepository;
+        this.blobService = blobService;
         this.objectMapper = objectMapper;
     }
 
@@ -31,9 +36,6 @@ public class FigureService {
     }
 
     private FigureDto convertEntityToDto(Figure figure) {
-        var pictureBlobs = figure.getPictures();
-        var pictures = pictureBlobs.stream().map(picture -> Base64.getEncoder().encodeToString(
-                picture.getImage())).toList();
 
         return new FigureDto(figure.getName(), figure.getDescription(),
                 figure.getBrand(),
@@ -41,22 +43,40 @@ public class FigureService {
                 figure.getLength(), figure.getWeight(), figure.getSeller());
     }
 
-    public List<ResponseDto> getAllFigures() {
+    public List<FigureDto> getAllFigures() {
         var figures = figureRepository.findAll();
         return figures.stream().map(
-                figure -> objectMapper.convertValue(figure, ResponseDto.class)).toList();
+                figure -> objectMapper.convertValue(figure, FigureDto.class)).toList();
     }
 
-    public ResponseDto getFigure(String id) {
+    public FigureDto getFigure(String id) {
         var figure = figureRepository.findById(id).orElseThrow(
                 () -> new NoSuchElementException("Figure not found"));
-        return objectMapper.convertValue(figure, ResponseDto.class);
+        return objectMapper.convertValue(figure, FigureDto.class);
     }
 
-    public ResponseDto addFigure(FigureDto request) {
-//        public FigureDto addFigure(String request, MultipartFile[] pictures) throws JsonProcessingException {
-//        FigureDto dto = objectMapper.readValue(request, FigureDto.class);
-        var figure = convertDtoToEntity(request);
+    public FigureDto addFigure(String figureDetails,
+                               MultipartFile[] pictures) {
+        Figure figure = null;
+        try {
+            figure = objectMapper.readValue(figureDetails, Figure.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        var savedPictures = Arrays.stream(pictures).map(picture -> {
+            var savedPicture = new Picture();
+            pictureRepository.save(savedPicture);
+                try {
+                    var blob = blobService.uploadBlob(savedPicture.getId(), picture);
+                    savedPicture.setFileType(picture.getContentType());
+                    savedPicture.setPictureUrl(blob);
+                    return savedPicture;
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }).toList();
+
+            figure.setPictures(savedPictures);
 
         figureRepository.save(figure);
 //        var savedPictures = pictureService.savePicturesAsBlobs(pictures, figure);
@@ -68,23 +88,5 @@ public class FigureService {
         var figure = figureRepository.findById(id).orElseThrow(
                 () -> new NoSuchElementException("Figure not found"));
         figureRepository.delete(figure);
-    }
-
-    public ResponseDto updateFigure(UpdateDto updateDto, String id) {
-        Figure figure = figureRepository.findById(id).orElseThrow(
-                () -> new NoSuchElementException("Figure not found"));
-        figure.setName(updateDto.name());
-        figure.setOrigin(updateDto.origin());
-        figure.setBrand(updateDto.brand());
-        figure.setWidth(updateDto.width());
-        figure.setLength(updateDto.length());
-        figure.setHeight(updateDto.height());
-        figure.setWeight(updateDto.weight());
-        figure.setDescription(updateDto.description());
-        figure.setPrice(updateDto.price());
-        figure.setRating(updateDto.rating());
-        figure.setCondition(updateDto.condition());
-
-        return objectMapper.convertValue(figureRepository.save(figure), ResponseDto.class);
     }
 }
