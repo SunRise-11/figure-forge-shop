4cb2b7903104d4da4454243ac461da94143c7aa3
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-23 17:50:49 +0100

    Add id field to ResponseDto

--- backend/src/main/java/com/typotitans/backend/dtos/ResponseDto.java
+++ backend/src/main/java/com/typotitans/backend/dtos/ResponseDto.java
@@ -5,6 +5,7 @@ import com.typotitans.backend.models.Picture;
 import java.util.List;
 
 public record ResponseDto(
+        String id,
         String name,
         String description,
         String condition,
