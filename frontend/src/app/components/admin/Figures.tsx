554d713e19af3d92cbc767de0c5fdbcee14504a6
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-23 19:46:22 +0100

    change scroll to table

--- frontend/src/app/components/admin/Figures.tsx
+++ frontend/src/app/components/admin/Figures.tsx
@@ -26,52 +26,54 @@ const Figures = ({ action, data }: Props) => {
     }
   }
   return (
-    <Card className="absolute right-0 top-[6rem] w-full max-w-[calc(100vw-19rem)] overflow-y-auto h-[calc(100vh-6rem)] p-4 shadow-xl shadow-blue-gray-900/5 border-solid border-2 ">
+    <Card className="absolute right-0 top-[6rem] w-full max-w-[calc(100vw-19rem)]  h-[calc(100vh-6rem)] p-4 shadow-xl shadow-blue-gray-900/5 border-solid border-2 ">
       <Typography variant="h5" color="blue-gray">
         {action} Figures
       </Typography>
-      <table className="w-full text-sm text-left text-black mt-10">
-        <thead className="border text-xs text-black uppercase bg-slate-300 dark:bg-slate-300 dark:text-black">
-          <tr className="bg-primary text-text">
-            <th scope="col" className="border px-6 py-3 w-4/5">
-              Name
-            </th>
-            <th scope="col" className="border px-6 py-3 text-center">
-              Action
-            </th>
-          </tr>
-          {data.map((toy) => {
-            return (
-              <tr
-                key={toy.id.toString()}
-                className="even:bg-white odd:bg-gray-100"
-              >
-                <td className="border px-6 py-4">{toy.name}</td>
-                <td className="border px-6 py-4 text-center">
-                  {action == "uncheck" ? (
-                    <Link
-                      href={`/filteredfigures/${action}/review/${toy.id}`}
-                      passHref
-                    >
-                      <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+      <div className="overflow-y-auto">
+        <table className="w-full text-sm text-left text-black mt-10 ">
+          <thead className="border text-xs text-black uppercase bg-slate-300 dark:bg-slate-300 dark:text-black">
+            <tr className="bg-primary text-text">
+              <th scope="col" className="border px-6 py-3 w-4/5">
+                Name
+              </th>
+              <th scope="col" className="border px-6 py-3 text-center">
+                Action
+              </th>
+            </tr>
+            {data.map((toy) => {
+              return (
+                <tr
+                  key={toy.id.toString()}
+                  className="even:bg-white odd:bg-gray-100"
+                >
+                  <td className="border px-6 py-4">{toy.name}</td>
+                  <td className="border px-6 py-4 text-center">
+                    {action == "uncheck" ? (
+                      <Link
+                        href={`/filteredfigures/${action}/review/${toy.id}`}
+                        passHref
+                      >
+                        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                          {buttonAction}
+                        </button>
+                      </Link>
+                    ) : action == "posted" ? (
+                      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                         {buttonAction}
                       </button>
-                    </Link>
-                  ) : action == "posted" ? (
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                      {buttonAction}
-                    </button>
-                  ) : (
-                    <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-                      {buttonAction}
-                    </button>
-                  )}
-                </td>
-              </tr>
-            );
-          })}
-        </thead>
-      </table>
+                    ) : (
+                      <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                        {buttonAction}
+                      </button>
+                    )}
+                  </td>
+                </tr>
+              );
+            })}
+          </thead>
+        </table>
+      </div>
     </Card>
   );
 };
--- frontend/src/app/components/admin/allfigures.tsx
+++ frontend/src/app/components/admin/allfigures.tsx
@@ -5,12 +5,6 @@ import { FiguresContext } from "@/app/contexts/figures.context";
 
 const AllFiguresComponent = () => {
   const { toys } = useContext(FiguresContext);
-  const [currentPage, setCurrentPage] = useState<number>(1);
-  const elementsPerPage: number = 9;
-
-  const handlePageChange = (pageNumber: number) => {
-    setCurrentPage(pageNumber);
-  };
 
   const formatter = new Intl.NumberFormat("en-US", {
     style: "currency",
@@ -18,48 +12,46 @@ const AllFiguresComponent = () => {
     minimumFractionDigits: 0,
   });
 
-  const startIndex = (currentPage - 1) * elementsPerPage;
-  const endIndex = startIndex + elementsPerPage;
-  const displayedToys = toys.slice(startIndex, endIndex);
-
   return (
-    <Card className="absolute right-0 w-full max-w-[calc(100vw-19rem)] top-[17rem] overflow-y-auto h-min max-h-[calc(100vh-17rem)] p-4 shadow-xl shadow-blue-gray-900/5 border-solid border-2 ">
+    <Card className="absolute right-0 w-full max-w-[calc(100vw-19rem)] top-[17rem]  h-min max-h-[calc(100vh-17rem)] p-4 shadow-xl shadow-blue-gray-900/5 border-solid border-2 ">
       <Typography variant="h5" color="blue-gray" className=" my-auto ">
         All Figures
       </Typography>
-      <table className="w-full text-sm text-left text-black px-15  h-full">
-        <thead className="border text-xs text-black uppercase bg-slate-300 dark:bg-slate-300 dark:text-black">
-          <tr className="bg-primary text-text">
-            <th scope="col" className="border px-6 py-3">
-              Name
-            </th>
-            <th scope="col" className="border px-6 py-3">
-              Status
-            </th>
-            <th scope="col" className="border px-6 py-3">
-              Rating
-            </th>
-            <th scope="col" className="border px-6 py-3">
-              Price
-            </th>
-          </tr>
-          {displayedToys.map((toy) => {
-            return (
-              <tr
-                key={toy.id.toString()}
-                className="even:bg-white odd:bg-gray-100"
-              >
-                <td className="border px-6 py-4">{toy.name}</td>
-                <td className="border px-6 py-4">{toy.status}</td>
-                <td className="border px-6 py-4">{toy.rating}</td>
-                <td className="border px-6 py-4">
-                  {formatter.format(toy.price)}
-                </td>
-              </tr>
-            );
-          })}
-        </thead>
-      </table>
+      <div className="overflow-y-auto">
+        <table className="w-full text-sm text-left text-black px-15  h-full">
+          <thead className="border text-xs text-black uppercase bg-slate-300 dark:bg-slate-300 dark:text-black">
+            <tr className="bg-primary text-text">
+              <th scope="col" className="border px-6 py-3">
+                Name
+              </th>
+              <th scope="col" className="border px-6 py-3">
+                Status
+              </th>
+              <th scope="col" className="border px-6 py-3">
+                Rating
+              </th>
+              <th scope="col" className="border px-6 py-3">
+                Price
+              </th>
+            </tr>
+            {toys.map((toy) => {
+              return (
+                <tr
+                  key={toy.id.toString()}
+                  className="even:bg-white odd:bg-gray-100"
+                >
+                  <td className="border px-6 py-4">{toy.name}</td>
+                  <td className="border px-6 py-4">{toy.status}</td>
+                  <td className="border px-6 py-4">{toy.rating}</td>
+                  <td className="border px-6 py-4">
+                    {formatter.format(toy.price)}
+                  </td>
+                </tr>
+              );
+            })}
+          </thead>
+        </table>
+      </div>
     </Card>
   );
 };
