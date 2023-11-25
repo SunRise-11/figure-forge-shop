c19d9ba3d9058adb7ee7752849e9bd7d95c81439
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-25 11:05:07 +0100

    change topbar name

--- frontend/src/app/components/admin/Topbar.tsx
+++ frontend/src/app/components/admin/Topbar.tsx
@@ -6,10 +6,17 @@ import React from "react";
 const Topbar = () => {
   const pathname = usePathname();
   const path = pathname.split("/");
+  let label = path[path.length - 1];
+
+  const topLabel = ["dashboard", "uncheck", "posted", "sold"];
+  if (!topLabel.includes(label)) {
+    label = "figure";
+  }
+
   return (
     <Card className="absolute top-0 right-0 w-full max-w-[calc(100vw-19rem)] h-20 p-4 shadow-xl shadow-blue-gray-900/5 border-solid border-2">
       <Typography variant="h2" color="blue-gray" className=" my-auto">
-        {path[path.length - 1]}
+        {label}
       </Typography>
     </Card>
   );
