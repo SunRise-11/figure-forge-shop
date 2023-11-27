f0a6dfa6db13f444204ef52ba02585460fc9f094
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-27 13:31:30 +0100

    change directory

--- frontend/src/app/components/public/Carousel.tsx
+++ frontend/src/app/components/public/Carousel.tsx
@@ -1,24 +1,34 @@
 import React from "react";
 import { Carousel } from "@material-tailwind/react";
 import Image from "next/image";
+import HeroImage from "../../../../public/hero-image.jpg";
 
 const CarouselHomePage = () => {
   return (
     <Carousel className="rounded-xl z-10" autoplay>
       <Image
-        src="../hero-image.jpg"
+        src={HeroImage}
+        width={400}
+        height={400}
         alt="image 1"
         className="h-full w-full object-cover"
+        priority
       />
       <Image
-        src="../hero-image.jpg"
+        src={HeroImage}
+        width={400}
+        height={400}
         alt="image 2"
         className="h-full w-full object-cover"
+        priority
       />
       <Image
-        src="../hero-image.jpg"
+        src={HeroImage}
+        width={400}
+        height={400}
         alt="image 3"
         className="h-full w-full object-cover"
+        priority
       />
     </Carousel>
   );
