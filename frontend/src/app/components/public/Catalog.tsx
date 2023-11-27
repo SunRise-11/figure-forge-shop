a76e4dde519e6321870717c17498197560375ef4
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-27 14:29:02 +0100

    change classnames of searchbar

--- frontend/src/app/components/public/Catalog.tsx
+++ frontend/src/app/components/public/Catalog.tsx
@@ -18,7 +18,7 @@ const Catalog = () => {
   const endIndex = startIndex + elementsPerPage;
   let displayedToys = toys.slice(startIndex, endIndex);
 
-  displayedToys = displayedToys.filter( 
+  displayedToys = displayedToys.filter(
     (figure) =>
       (figure.status == "posted" &&
         figure.name.toLowerCase().includes(searchFigure.toLowerCase())) ||
@@ -27,34 +27,33 @@ const Catalog = () => {
 
   return (
     <>
-     <div className="relative mt-5">
-          <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-            <svg
-              className="w-4 h-4 text-gray-500 dark:text-gray-400"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 20 20"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-              />
-            </svg>
-          </div>
-          <input
-            type="search"
-            onChange={(e) => setSearchFigure(e.target.value)}
-            className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Search Mockups, Logos..."
-            required
-          />
-        </div> 
+      <div className="relative mt-5">
+        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+          <svg
+            className="w-4 h-4 text-gray-500 dark:text-gray-400"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+            />
+          </svg>
+        </div>
+        <input
+          type="search"
+          onChange={(e) => setSearchFigure(e.target.value)}
+          className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Search Mockups, Logos..."
+          required
+        />
+      </div>
       <div className="flex flex-wrap w-full">
-       
         {displayedToys.map((toy, index) => (
           <div
             className="flex w-full sm:w-1/1  md:w-1/3 lg:w-1/3 pw-10 py-4 justify-center items-center"
