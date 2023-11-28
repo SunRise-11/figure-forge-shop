366369803253b0d2f87e87d38374b031d4c74861
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-28 08:52:37 +0100

    Rename SeedingService to DatabaseMocker

--- backend/src/main/java/com/typotitans/backend/mocking/DatabaseMocker.java
+++ backend/src/main/java/com/typotitans/backend/mocking/DatabaseMocker.java
@@ -1,13 +1,14 @@
-package com.typotitans.backend.services;
+package com.typotitans.backend.mocking;
 
 import com.typotitans.backend.models.Figure;
 import com.typotitans.backend.models.Picture;
 import com.typotitans.backend.repositories.FigureRepository;
 import com.typotitans.backend.repositories.PictureRepository;
+import com.typotitans.backend.services.BlobService;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.util.ResourceUtils;
 
 import java.io.BufferedReader;
@@ -16,15 +17,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-@Service
+@Component
 @Profile("dev")
-public class SeedingService implements ApplicationRunner {
+public class DatabaseMocker implements ApplicationRunner {
 
     private final FigureRepository figureRepository;
     private final PictureRepository pictureRepository;
     private final BlobService blobService;
 
-    public SeedingService(FigureRepository figureRepository, PictureRepository pictureRepository,
+    public DatabaseMocker(FigureRepository figureRepository, PictureRepository pictureRepository,
                           BlobService blobService) {
         this.figureRepository = figureRepository;
         this.pictureRepository = pictureRepository;
@@ -35,11 +36,13 @@ public class SeedingService implements ApplicationRunner {
     public void run(ApplicationArguments args) {
         SeedFromCsv();
 
-    } public List<Picture> seedPictures(int index, Figure figure) {
+    }
+
+    private List<Picture> seedPictures(int index, Figure figure) {
         List<Picture> pictures = new ArrayList<>();
         try {
             BufferedReader reader = new BufferedReader(new FileReader(
-                    ResourceUtils.getFile("src/main/resources/mock-pictures.csv")));
+                    ResourceUtils.getFile("src/main/resources/data/mock-pictures.csv")));
 
             String line;
             boolean isFirstLine = true;
@@ -66,10 +69,10 @@ public class SeedingService implements ApplicationRunner {
         return pictures;
     }
 
-    public void SeedFromCsv() {
+    private void SeedFromCsv() {
         try {
             BufferedReader reader = new BufferedReader(new FileReader(
-                    ResourceUtils.getFile("src/main/resources/mock-data.csv")));
+                    ResourceUtils.getFile("src/main/resources/data/mock-data.csv")));
 
             String line;
             int index = 1;
@@ -90,12 +93,12 @@ public class SeedingService implements ApplicationRunner {
                 figure.setHeight(Integer.parseInt(data[7]));
                 figure.setWeight(Integer.parseInt(data[8]));
 
-                String description = String.join(" ",Arrays.copyOfRange(data, 9, data.length));
-                description = description.replace("\"","");
+                String description = String.join(" ", Arrays.copyOfRange(data, 9, data.length));
+                description = description.replace("\"", "");
 
                 figure.setDescription(description);
                 figureRepository.save(figure);
-                figure.setPictures(seedPictures(index,figure));
+                figure.setPictures(seedPictures(index, figure));
 
                 figureRepository.save(figure);
 
