d47c0ce959d490542777d725a6b4c690da9c7791
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-29 15:21:17 +0100

    Add buy button

--- frontend/src/app/(public)/figures/[id]/page.tsx
+++ frontend/src/app/(public)/figures/[id]/page.tsx
@@ -1,9 +1,11 @@
-"use client";
-import React, { useContext, useEffect, useState } from "react";
-import Rating from "@/app/components/public/Rating";
-import { FiguresContext } from "@/app/contexts/figures.context";
-import { Toy } from "@/app/types/types";
-import CarouselDetail from "@/app/components/public/CarouselDetail";
+'use client';
+import React, { useContext, useEffect, useState } from 'react';
+import Rating from '@/app/components/public/Rating';
+import { FiguresContext } from '@/app/contexts/figures.context';
+import { Toy } from '@/app/types/types';
+import CarouselDetail from '@/app/components/public/CarouselDetail';
+import { useShoppingCart } from '@/app/contexts/cartContext';
+import { ShoppingCartIcon, XMarkIcon } from '@heroicons/react/24/outline';
 
 type Props = {
   params: { id: string };
@@ -12,6 +14,11 @@ type Props = {
 const DetailPage = ({ params }: Props) => {
   const [data, setData] = useState<Toy | undefined>(undefined);
   const { toys } = useContext(FiguresContext);
+
+  const { getItemQuantity, increaseCartQuantity, removeFromCart } =
+    useShoppingCart();
+
+  let quantity = getItemQuantity(params.id);
 
   useEffect(() => {
     const fetchBackend = async () => {
@@ -28,12 +35,34 @@ const DetailPage = ({ params }: Props) => {
     return (
       <>
         <CarouselDetail pictures={data.pictures} />
-        <div className="w-full h-20 bg-primary text-text text-4xl flex justify-center items-center">
+        <div className="w-full h-20 bg-background text-text text-4xl flex justify-center items-center">
           {data.name}
         </div>
+
+        <div className="flex justify-center my-5">
+          {quantity === 0 ? (
+            <button
+              className="lg:inline-flex lg:w-auto px-3 py-2 rounded text-text bg-primary font-medium flex flex-row justify-center transition ease-in-out delay-350 hover:bg-secondary hover:transition-all"
+              onClick={() => increaseCartQuantity(params.id)}
+            >
+              <ShoppingCartIcon className="h-6 text-text" />
+              &nbsp;Add to cart
+            </button>
+          ) : (
+            <button
+              className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-text bg-red-600 font-medium text-center items-center justify-center transition ease-in-out delay-350 hover:bg-red-800 hover:transition-all"
+              onClick={() => removeFromCart(params.id)}
+            >
+              <XMarkIcon className="h-6 text-text" />
+              &nbsp;Remove from cart
+            </button>
+          )}
+        </div>
         <div className="flex flex-col sm:flex-row h-max mb-4 py-10">
-          <div className="flex flex-col sm:items-start sm:justify-center py-4 gap-10 w-1/2">
-            <legend className="text-lg text-text font-semibold">Details</legend>
+          <div className="flex flex-col items-start md:items-center sm:justify-center py-4 gap-10 w-1/2">
+            <legend className="text-lg text-text font-semibold flex justify-start md:justify-center w-full">
+              Details
+            </legend>
             <div className="w-full max-w-sm">
               <div className="md:flex md:items-center mb-6">
                 <div className="md:w-1/3">
@@ -94,8 +123,8 @@ const DetailPage = ({ params }: Props) => {
               </div>
             </div>
           </div>
-          <div className="flex flex-col sm:items-start sm:justify-center py-4 gap-10 w-1/2">
-            <legend className="text-lg text-text font-semibold">
+          <div className="flex flex-col items-start md:items-center sm:justify-center py-4 gap-10 w-1/2">
+            <legend className="text-lg text-text font-semibold flex justify-start md:justify-center w-full">
               Dimension
             </legend>
             <div className="w-full max-w-sm">
@@ -159,36 +188,20 @@ const DetailPage = ({ params }: Props) => {
             </div>
           </div>
         </div>
-        <div className="flex flex-col sm:flex-row h-max mb-4">
-          <div className="flex flex-row flex-wrap -mx-3 mb-6 w-full">
-            <div className="w-full px-3">
-              <legend className="text-lg text-text font-semibold">
-                Description
-              </legend>
-              <div
-                className="appearance-none block w-screen sm:w-full bg-gray-200 text-background border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="grid-description"
-                placeholder="description"
-              >
-                {data.description}
-              </div>
-            </div>
+        <div className="flex flex-col items-start justify-center md:items-center h-max mb-4 mx-auto">
+          <legend className="text-lg text-text font-semibold mb-4">
+            Description
+          </legend>
+          <div className="appearance-none block w-screen sm:w-4/5 bg-gray-200 text-background border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+            {data.description}
           </div>
         </div>
-        <div className="flex flex-col sm:flex-row h-max mb-4">
-          <div className="flex flex-row flex-wrap -mx-3 mb-6 w-full">
-            <div className="w-full px-3">
-              <legend className="text-lg text-text font-semibold">
-                Conditions
-              </legend>
-              <div
-                className="appearance-none block w-screen sm:w-full bg-gray-200 text-background border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 min-h-full"
-                id="grid-description"
-                placeholder="description"
-              >
-                {data.conditions}
-              </div>
-            </div>
+        <div className="flex flex-col items-start justify-center md:items-center h-max mb-4 mx-auto">
+          <legend className="text-lg text-text font-semibold mb-4">
+            Conditions
+          </legend>
+          <div className="appearance-none block w-screen sm:w-4/5 bg-gray-200 text-background border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+            {data.conditions}
           </div>
         </div>
 
