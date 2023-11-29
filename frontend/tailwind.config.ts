d1f24b46a4ca04f0ebfe7fa8241cee001753ffa5
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-29 14:38:03 +0100

    Change secondary color

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -2,32 +2,28 @@ import type { Config } from "tailwindcss";
 const withMT = require("@material-tailwind/react/utils/withMT");
  
 module.exports = withMT({
- 
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}", 
-
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     colors: {
-      text: "#E8E8E8",
-      background: "#0B0411",
-      primary: "#60319C",
-      secondary: "#262627",
-      accent: "#8C8D8B",
+      text: '#E8E8E8',
+      background: '#0B0411',
+      primary: '#60319C',
+      secondary: '#8b4ddb',
+      accent: '#8C8D8B',
     },
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [ 
-  ],
-}
-);
+  plugins: [],
+});
  
  
