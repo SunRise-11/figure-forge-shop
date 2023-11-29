79dbdc88261f16e496f1354080c0d4e09fa13ad5
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-11-29 08:56:33 +0100

    add more pisctures to carousel

--- frontend/src/app/components/public/Carousel.tsx
+++ frontend/src/app/components/public/Carousel.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Carousel } from "@material-tailwind/react";
 import Image from "next/image";
-import HeroImage from "../../../../public/hero-image.jpg";
+import HeroImage1 from "../../../../public/hero-image.jpg";
+import HeroImage2 from "../../../../public/Slide1.png"
+import HeroImage3 from "../../../../public/Slide2.png"
 
 const CarouselHomePage = () => {
   return (
     <Carousel className="rounded-xl z-10" autoplay>
       <Image
-        src={HeroImage}
+        src={HeroImage1}
         width={400}
         height={400}
         alt="image 1"
@@ -15,7 +17,7 @@ const CarouselHomePage = () => {
         priority
       />
       <Image
-        src={HeroImage}
+        src={HeroImage2}
         width={400}
         height={400}
         alt="image 2"
@@ -23,7 +25,7 @@ const CarouselHomePage = () => {
         priority
       />
       <Image
-        src={HeroImage}
+        src={HeroImage3}
         width={400}
         height={400}
         alt="image 3"
